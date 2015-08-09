0f7037f1b5c952de62efe6c17f936dde627bbce2
Izzette <aircowpeacock@gmail.com>
2015-08-09 10:08:10 -0500

    v0.9.3 -- Fixed lint warnings

--- UnanimusProject/src/com/sjgilbert/unanimus/UnanimusGroup.java
+++ UnanimusProject/src/com/sjgilbert/unanimus/UnanimusGroup.java
@@ -17,13 +17,31 @@ public class UnanimusGroup extends ParseObject {
     }
 
     public ArrayList<ParseUser> getMembers() {
-        if (get("members") != null) {
-            return (ArrayList<ParseUser>) get("members");
-        } else return null;
+        Object o_members = get("members");
+
+        if (null == o_members)
+            throw new NullPointerException();
+
+        if (! (o_members instanceof ArrayList))
+            throw new ClassCastException();
+
+        ArrayList al_members = (ArrayList) o_members;
+
+        ArrayList<ParseUser> al_p_members = new ArrayList<>();
+        for (Object o : al_members) if (o instanceof ParseUser)
+                al_p_members.add((ParseUser) o);
+
+        return al_p_members;
     }
 
     public ParseUser getMember(int ind) {
-        ArrayList<ParseUser> members = (ArrayList<ParseUser>) get("members");
+        ArrayList<ParseUser> members;
+        try {
+            members = getMembers();
+        } catch (NullPointerException | ClassCastException e) {
+            e.printStackTrace();
+            members = new ArrayList<>();
+        }
         return members.get(ind);
     }
 
