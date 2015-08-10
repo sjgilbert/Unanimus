30012d13bf7740b8ec7989bd462985094775ec63
sjgilbert <samjgilbert94@gmail.com>
2015-08-10 16:30:51 -0500

    refactored FriendPickerActivity

--- UnanimusProject/src/com/sjgilbert/unanimus/FriendPickerActivity.java
+++ UnanimusProject/src/com/sjgilbert/unanimus/FriendPickerActivity.java
@@ -1,5 +1,6 @@
 package com.sjgilbert.unanimus;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +9,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.parse.ParseUser;
 import com.sjgilbert.unanimus.unanimus_activity.UnanimusActivityTitle;
 
 import org.json.JSONArray;
@@ -29,7 +30,7 @@ import java.util.List;
  * Custom ArrayList adapter idea for ListView fount at http://www.learn2crack.com/2013/10/android-custom-listview-images-text-example.html
  */
 public class FriendPickerActivity extends UnanimusActivityTitle {
-    private ArrayList<String> groupMembersFacebookIDs;
+    private FpaContainer fpaContainer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,15 +43,7 @@ public class FriendPickerActivity extends UnanimusActivityTitle {
             e.printStackTrace();
         }
 
-        groupMembersFacebookIDs = new ArrayList<>();
-
-        Button doneButton = (Button) findViewById(R.id.fpa_done_button);
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO:whatever needs to be done with groupMembersFacebookIDs
-            }
-        });
+        fpaContainer = new FpaContainer();
 
         //The request for facebook friends
         GraphRequest.newMyFriendsRequest(
@@ -98,21 +91,66 @@ public class FriendPickerActivity extends UnanimusActivityTitle {
                                     ) {
                                         final String idp = ids.get(position);
                                         final int color;
-                                        if (groupMembersFacebookIDs.contains(idp)) {
-                                            groupMembersFacebookIDs.remove(idp);
+                                        if (fpaContainer.facebookIDs.contains(idp)) {
+                                            removeFacebookID(idp);
                                             color = Color.TRANSPARENT;
                                         } else {
-                                            groupMembersFacebookIDs.add(idp);
+                                            addFacebookID(idp);
                                             color = Color.argb(127, 0, 0, 255);
                                         }
 
                                         view.setBackgroundColor(color);
-                                        System.out.println(groupMembersFacebookIDs.toString());
                                     }
                                 }
                         );
                     }
                 }
         ).executeAsync();
+
+        Button doneButton = (Button) findViewById(R.id.fpa_done_button);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnIntentFinish();
+            }
+        });
+
     }
+
+    private void addFacebookID(String friendID) {
+            fpaContainer.facebookIDs.add(friendID);
+    }
+
+    private void removeFacebookID(String friendID) {
+        fpaContainer.facebookIDs.remove(friendID);
+    }
+
+    private void returnIntentFinish() {
+        Intent intent = new Intent();
+        intent.putExtra("fpaContainer", fpaContainer.getAsBundle());
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    protected static class FpaContainer {
+        public final static String FACEBOOK_IDS="facebookIDs";
+
+        private ArrayList<String> facebookIDs;
+
+        public FpaContainer() {
+            final String userFacebookID = ParseUser.getCurrentUser().getString("facebookID");
+
+            facebookIDs = new ArrayList<>();
+        }
+
+        public Bundle getAsBundle() {
+            Bundle bundle = new Bundle();
+            bundle.putStringArrayList(FACEBOOK_IDS, facebookIDs);
+
+            return bundle;
+        }
+
+        public ArrayList<String> getFacebookIDs() {return facebookIDs;}
+    }
+
 }
