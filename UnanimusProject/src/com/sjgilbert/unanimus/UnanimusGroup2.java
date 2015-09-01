d9e1218d1a1ff7fdbe630230ea8cd364d01e1579
sjgilbert <samjgilbert94@gmail.com>
2015-09-01 11:55:31 -0500

    refactored

--- UnanimusProject/src/com/sjgilbert/unanimus/UnanimusGroup2.java
+++ UnanimusProject/src/com/sjgilbert/unanimus/UnanimusGroup2.java
@@ -1,11 +1,19 @@
 package com.sjgilbert.unanimus;
 
+import android.support.annotation.NonNull;
+
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.AbstractMap;
+import java.util.AbstractSet;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by sam on 9/1/15.
@@ -15,6 +23,12 @@ public class UnanimusGroup2 extends ParseObject {
 
     public static ParseQuery<UnanimusGroup2> getQuery() {
         return ParseQuery.getQuery(UnanimusGroup2.class);
+    }
+
+    public UnanimusGroup2(Collection<String> voteContainerIds, Collection<String> userIds, Collection<String> restaurantIds) {
+        addAll("voteContainerIds", voteContainerIds);
+        addAll("userIds", userIds);
+        addAll("restaurantIds", restaurantIds);
     }
 
     public List<String> getVoteContainerIds() {
@@ -29,16 +43,64 @@ public class UnanimusGroup2 extends ParseObject {
         return getList("restaurantIds");
     }
 
-    public void setVoteContainerIds(Collection<String> voteContainerIds) {
-        addAll("voteContainerIds", voteContainerIds);
-    }
+    public class VoteContainerMap extends ParseObject implements Map<String, VoteContainer> {
+        private final Map<String, VoteContainer> vcm = new HashMap<>();
 
-    public void setUserIds(Collection<String> userIds) {
-        addAll("userIds", userIds);
-    }
+        @Override
+        public void clear() {
+            vcm.clear();
+        }
 
-    public void setRestaurantIds(Collection<String> restaurantIds) {
-        addAll("restaurantIds", restaurantIds);
-    }
+        @Override
+        public boolean containsKey(Object key) {
+            return vcm.containsKey(key);
+        }
 
+        @Override
+        public boolean containsValue(Object value) {
+            return vcm.containsValue(value);
+        }
+
+        @NonNull
+        @Override
+        public Set<Entry<String, VoteContainer>> entrySet() {
+            return vcm.entrySet();
+        }
+
+        @Override
+        public VoteContainer get(Object key) {
+            return vcm.get(key);
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return vcm.isEmpty();
+        }
+
+        @Override
+        public VoteContainer put(String key, VoteContainer value) {
+            return vcm.put(key, value);
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends VoteContainer> map) {
+            vcm.putAll(map);
+        }
+
+        @Override
+        public VoteContainer remove(Object key) {
+            return vcm.remove(key);
+        }
+
+        @Override
+        public int size() {
+            return vcm.size();
+        }
+
+        @NonNull
+        @Override
+        public Collection<VoteContainer> values() {
+            return vcm.values();
+        }
+    }
 }
