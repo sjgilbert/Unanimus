e827847d10e7ad3b98a07a877c533eb56366b330
Izzette <aircowpeacock@gmail.com>
2015-09-06 14:50:37 -0500

    tmp

---
+++ UnanimusProject/src/com/sjgilbert/unanimus/IDependencyContainer.java
@@ -0,0 +1,30 @@
+package com.sjgilbert.unanimus;
+
+import android.os.Bundle;
+
+/**
+ * 9/6/15 (c) Isabell Cowan
+ * isabellcowan@gmail.com
+ */
+interface IDependencyContainer {
+    Bundle getAsBundle() throws NotSetException;
+    void commit() throws  NotSetException;
+    void setDefault() throws NotSetException;
+    void setFromBundle(Bundle bundle) throws NotSetException;
+    boolean isSet();
+
+    class NotSetException extends IllegalStateException {
+        protected NotSetException() {
+            super();
+        }
+
+        protected NotSetException(String message) {
+            super(message);
+        }
+
+        protected NotSetException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
+
---
+++ UnanimusProject/src/com/sjgilbert/unanimus/PpaContainer.java
@@ -0,0 +1,92 @@
+package com.sjgilbert.unanimus;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.sjgilbert.unanimus.parsecache.ParseCache;
+
+/**
+ * 9/6/15 (c) Isabell Cowan
+ * isabellcowan@gmail.com
+ */
+@ParseClassName("PpaContainer")
+public class PpaContainer extends ParseObject implements  IDependencyContainer {
+    private static final String LAT = "lat";
+    private static final String LNG = "lng";
+
+    private LatLng latLng;
+
+    public PpaContainer() {
+        super();
+
+        if (! has(LAT) || ! has(LNG))
+            return;
+
+        int lat = getInt(LAT);
+        int lng = getInt(LNG);
+
+        this.latLng = new LatLng(lat, lng);
+    }
+
+    @Override
+    public Bundle getAsBundle() throws NotSetException {
+        if (! isSet())
+            throw new NotSetException();
+
+        Bundle bundle = new Bundle();
+        bundle.putDouble(LAT, latLng.latitude);
+        bundle.putDouble(LNG, latLng.longitude);
+
+        bundle.putString(ParseCache.OBJECT_ID, getObjectId());
+
+        return bundle;
+    }
+
+    @Override
+    public void commit() throws NotSetException {
+        if (! isSet())
+            throw new NotSetException();
+
+        put(LAT, latLng.latitude);
+        put(LNG, latLng.longitude);
+    }
+
+    @Override
+    public void setDefault() {
+        latLng = new LatLng(0D, 0D);
+    }
+
+    @Override
+    public void setFromBundle(Bundle bundle) throws NotSetException {
+        setFromLatLng(
+                new LatLng(
+                        bundle.getDouble(LAT),
+                        bundle.getDouble(LNG)
+                )
+        );
+
+        setObjectId(bundle.getString(ParseCache.OBJECT_ID));
+        commit();
+    }
+
+    @Override
+    public boolean isSet() {
+        return null != latLng;
+    }
+
+    @Deprecated
+    void setLatLng(Double lat, Double lng) {
+        setFromLatLng(new LatLng(lat, lng));
+    }
+
+    void setFromLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    LatLng getLatLng() {
+        return latLng;
+    }
+}
+
