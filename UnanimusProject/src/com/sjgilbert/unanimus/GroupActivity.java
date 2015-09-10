83427580d4c904ce360b5790b4afa875661c4202
Izzette <aircowpeacock@gmail.com>
2015-09-10 10:37:18 -0500

    Group activity ui enhancements

--- UnanimusProject/src/com/sjgilbert/unanimus/GroupActivity.java
+++ UnanimusProject/src/com/sjgilbert/unanimus/GroupActivity.java
@@ -1,10 +1,12 @@
 package com.sjgilbert.unanimus;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,19 +15,17 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphRequestBatch;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
-import com.facebook.Profile;
-import com.parse.GetCallback;
-import com.parse.Parse;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.SaveCallback;
 import com.sjgilbert.unanimus.parsecache.ParseCache;
 import com.sjgilbert.unanimus.unanimus_activity.UnanimusActivityTitle;
 
 import org.json.JSONException;
 
 import java.util.ArrayList;
+
+import bolts.Continuation;
+import bolts.Task;
 
 /**
  * The activity which displays a specific group_activity a user is a part of.  Should
@@ -67,30 +67,46 @@ public class GroupActivity extends UnanimusActivityTitle {
 
         ParseCache.parseCache.put(unanimusGroupId, (ParseQuery<ParseObject>) query);
 
-        ((ParseQuery<UnanimusGroup>) query).getFirstInBackground(new GetCallback<UnanimusGroup>() {
+        ((ParseQuery<UnanimusGroup>) query).getFirstInBackground().onSuccess(new Continuation<UnanimusGroup, UnanimusGroup>() {
             @Override
-            public void done(UnanimusGroup unanimusGroup, ParseException e) {
-                if (e != null) log(ELog.e, e.getMessage(), e);
-
-                try {
-                    unanimusGroup.load();
-                } catch (ParseException e1) {
-                    log(ELog.e, e1.getMessage(), e1);
-                    return;
-                }
+            public UnanimusGroup then(Task<UnanimusGroup> task) throws Exception {
+                final UnanimusGroup unanimusGroup = task.getResult();
+                unanimusGroup.load();
 
                 GroupActivity.this.unanimusGroup = unanimusGroup;
 
                 CgaContainer cgaContainer = unanimusGroup.getCgaContainer();
                 FpaContainer fpaContainer = cgaContainer.getFpaContainer();
 
-                if (fpaContainer != null) {
-                    FpaContainer.UserIdPair[] userIdPairs = fpaContainer.getUserIdPairs();
-                    setData(userIdPairs);
-                }
+                assert fpaContainer != null;
+                final FpaContainer.UserIdPair[] userIdPairs = fpaContainer.getUserIdPairs();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setData(userIdPairs);
+                    }
+                });
+
+                return null;
+            }
+        }).onSuccessTask(new Continuation<UnanimusGroup, Task<Void>>() {
+            @Override
+            public Task<Void> then(Task<UnanimusGroup> task) throws Exception {
+                return refreshButtons(unanimusGroup);
+            }
+        }).continueWith(new Continuation<Void, Void>() {
+            @Override
+            public Void then(Task<Void> task) throws Exception {
+                Exception e = task.getError();
+                if (e != null)
+                    log(ELog.e, e.getMessage(), e);
+                else
+                    log(ELog.i, "Successfully created");
+
+                return null;
             }
         });
-
     }
 
     private void setData(final FpaContainer.UserIdPair[] userIdPairs) {
@@ -150,12 +166,80 @@ public class GroupActivity extends UnanimusActivityTitle {
         startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (unanimusGroup != null)
+            refreshButtons(unanimusGroup);
+    }
+
+    private Task<Void> refreshButtons(UnanimusGroup unanimusGroup) {
+        return unanimusGroup.fetchInBackground().onSuccess(new Continuation<ParseObject, UnanimusGroup>() {
+            @Override
+            public UnanimusGroup then(Task<ParseObject> task) throws Exception {
+                final ParseObject result = task.getResult();
+                final UnanimusGroup unanimusGroup = (UnanimusGroup) result;
+
+                GroupActivity.this.unanimusGroup = unanimusGroup;
+
+                unanimusGroup.load();
+
+                return unanimusGroup;
+            }
+        }).onSuccess(new Continuation<UnanimusGroup, Void>() {
+            @Override
+            public Void then(Task<UnanimusGroup> task) throws Exception {
+                final UnanimusGroup unanimusGroup = task.getResult();
+
+                final Button playButton = (Button) findViewById(R.id.ga_play_button);
+                final Button recsButton = (Button) findViewById(R.id.ga_view_recs_button);
+
+                final boolean playable = unanimusGroup.hasNotVoted();
+                final boolean recsReady = unanimusGroup.allHaveVoted();
+
+                final Resources resources = getResources();
+
+                final int buttonColor = resources.getColor(R.color.button_color);
+                final int shadedColor = resources.getColor(R.color.button_shaded);
+
+                final int playColor = (playable) ? buttonColor : shadedColor;
+                final int recsColor = (recsReady) ? buttonColor : shadedColor;
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        playButton.setBackgroundColor(playColor);
+                        playButton.setClickable(playable);
+
+                        recsButton.setBackgroundColor(recsColor);
+                        recsButton.setClickable(recsReady);
+                    }
+                });
+                return null;
+            }
+        }).continueWith(new Continuation<Void, Void>() {
+            @Override
+            public Void then(Task<Void> task) throws Exception {
+                Exception e = task.getError();
+                if (e != null)
+                    log(ELog.e, e.getMessage(), e);
+                else
+                    log(ELog.i, "Successfully resumed");
+
+                return null;
+            }
+        });
+    }
+
     @SuppressWarnings("unused")
     public void ga_viewStartRecommendationActivity(@SuppressWarnings("UnusedParameters") View view) {
-        if (!unanimusGroup.getRecommendation().isEmpty()) {
-            Intent intent = new Intent(GroupActivity.this, RecommendationActivity.class);
-            intent.putExtra(GROUP_ID, unanimusGroupId);
-            startActivity(intent);
-        }
+        if (unanimusGroup.getRecommendation().isEmpty())
+            return;
+
+        Intent intent = new Intent(GroupActivity.this, RecommendationActivity.class);
+        intent.putExtra(GROUP_ID, unanimusGroupId);
+        startActivity(intent);
+        onPause();
     }
 }
